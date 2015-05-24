df57588586b9f7de2cd976938cc007b6351f0277
Andreas Wolf <dev@a-w.io>
2015-05-24 15:19:20 +0200

    [CLEANUP] Factor out initial SimulationStep constructor

--- src/main/scala/info/andreaswolf/roadhopper/simulation/SimulationStep.scala
+++ src/main/scala/info/andreaswolf/roadhopper/simulation/SimulationStep.scala
@@ -1,16 +1,13 @@
 package info.andreaswolf.roadhopper.simulation
 
+object SimulationStep {
+	def initial(vehicleState: VehicleState) = new SimulationStep(0, 0.0, vehicleState)
+}
+
 /**
  * 
  * @param time The point in time of this simulation step, in milliseconds (ms)
  */
 class SimulationStep(val time: Int, val position: Double, var vehicleState: VehicleState) {
-
-	/**
-	 * Default constructor: returns the first step with a vehicle without any speed
-	 *
-	 * TODO should we move this (with a more fitting name) into a class object?
-	 */
-	def this() = this(0, 0, new VehicleState(0, 0))
 
 }
