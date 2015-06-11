306603a27faa70eadb5afebec4482c7606497022
Andreas Wolf <dev@a-w.io>
2015-06-11 17:32:43 +0200

    [FEATURE] Create road segments from coordinates

--- src/main/scala/info/andreaswolf/roadhopper/road/RoadSegment.scala
+++ src/main/scala/info/andreaswolf/roadhopper/road/RoadSegment.scala
@@ -1,14 +1,40 @@
 package info.andreaswolf.roadhopper.road
 
+object RoadSegment {
+
+	/**
+	 * The earth’s radius as defined for WGS84.
+	 */
+	val R = 6371000
+
+	/**
+	 *
+	 * @return A RoadSegment instance created from the coordinates
+	 *
+	 * Note that this uses an approximation for the length that treats the line segment as being orthogonal to the
+	 * earth’s radius (Pythagorean approximation). This is generally considered safe for the short distances we have.
+	 * <http://www.movable-type.co.uk/scripts/latlong.html>
+	 */
+	def fromCoordinates(lat1: Double, lon1: Double, lat2: Double, lon2: Double) : RoadSegment = {
+		val phi1 = lat1.toRadians
+		val phi2 = lat2.toRadians
+		val deltaLambda = (lon2 - lon1).toRadians
+		val x = deltaLambda * Math.cos((phi1+phi2)/2)
+		val y = phi2 - phi1
+		val length = Math.sqrt(x*x + y*y) * R
+		val orientation = Math.atan2(Math.sin(lon2 - lon1) * Math.cos(lat2), Math.cos(lat1) * Math.sin(lat2) -
+			Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1)).toDegrees
+
+		new RoadSegment(length, orientation)
+	}
+
+}
+
 /**
  *
  * @param orientation The orientation in polar coordinates (-pi..+pi, 0 = east)
  */
 class RoadSegment(val length: Double, var orientation: Double = 0.0) extends RoutePart {
-	// correct orientation to be in (-pi..+pi]
-	orientation %= (Math.PI * 2)
-	if (orientation == -Math.PI)
-		orientation = -orientation
 
 	/**
 	 * Returns the angle necessary to get from this segment to the given segment
---
+++ src/test/scala/info/andreaswolf/roadhopper/road/RoadSegment$Test.scala
@@ -0,0 +1,32 @@
+package info.andreaswolf.roadhopper.road
+
+import org.scalatest.FunSuite
+import org.scalatest.prop.Tables.Table
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import junit.framework.Assert
+
+class RoadSegment$Test extends FunSuite {
+
+	// calculated using <http://www.movable-type.co.uk/scripts/latlong.html>
+	val coordinates = Table(
+		("lat1", "lon1", "lat2", "lon2", "length", "orientation"),
+		(  49.0,    8.0,   49.0,  8.001,    72.950, 90.0),
+		(  49.0,  8.001,   49.0,  8.0  ,    72.950, -90.0),
+		(  49.0,    8.0, 49.001,    8.0,   111.200,   0.0),
+		(49.001,    8.0,   49.0,    8.0,   111.200, 180.0)
+	)
+	forAll(coordinates) {
+		(x1: Double, y1: Double, x2: Double, y2: Double, expectedLength: Double, expectedOrientation: Double) =>
+		test("line from (%.4f,%.4f) to (%.4f,%.4f)" format (x1,y1,x2,y2)) {
+			val segment = RoadSegment.fromCoordinates(x1, y1, x2, y2)
+
+			if (expectedLength < Double.MaxValue) {
+				Assert.assertEquals(expectedLength, segment.length, 10e-3)
+			}
+			if (expectedOrientation < Double.MaxValue)
+				// TODO find a way to get a lower delta here
+				Assert.assertEquals(expectedOrientation, segment.orientation, 0.3)
+		}
+	}
+
+}
