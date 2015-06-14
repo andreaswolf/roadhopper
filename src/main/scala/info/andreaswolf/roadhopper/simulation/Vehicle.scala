85e9b0f0ad201f74c63fe7e97e7b6ca997e7eaaa
Andreas Wolf <dev@a-w.io>
2015-06-14 18:16:49 +0200

    [CLEANUP] Remove old vehicle code

--- src/main/scala/info/andreaswolf/roadhopper/simulation/Vehicle.scala
+++ src/main/scala/info/andreaswolf/roadhopper/simulation/Vehicle.scala
@@ -2,25 +2,6 @@ package info.andreaswolf.roadhopper.simulation
 
 import akka.actor.{Actor, ActorRef}
 
-/**
- * Simple representation of a vehicle
- *
- * @param maxAcceleration The maximum acceleration in meters per square second (m/(s^2^))
- * @param maxSpeed The maximum speed in meters per second
- */
-class Vehicle(val maxAcceleration: Float, val maxSpeed: Double) {
-
-	def calculateNewState(currentState: VehicleState, delta: Int): VehicleState = {
-		val speed = Math.min(maxSpeed, currentState.speed + (currentState.acceleration * delta / 1000))
-		var acceleration = 0.0
-		if (speed < maxSpeed) {
-			acceleration = currentState.acceleration
-		}
-
-		new VehicleState(acceleration, speed, 0.0, driverInput = new DriverInput(0.0))
-	}
-}
-
 case class Accelerate(value: Double)
 case class Decelerate(value: Double)
 case class SetAcceleration(value: Double)
