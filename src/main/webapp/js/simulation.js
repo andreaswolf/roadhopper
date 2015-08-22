6ebf1cbb2e5633fd6f3fef5244eaff56bf134d5b
Andreas Wolf <dev@a-w.io>
2015-08-22 23:30:18 +0200

    [BUGFIX] Simulation markers on map must be removed

--- src/main/webapp/js/simulation.js
+++ src/main/webapp/js/simulation.js
@@ -1,28 +1,25 @@
 /*
  * Copyright (c) 2015 Andreas Wolf
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * See te LICENSE file in the project root for further copyright information.
  */
 
 (function (roadhopper) {
 
 	var simulation = {
+		markerDrawn: false,
+
+		/**
+		 * The Leaflet.Playback instance used
+		 */
+		playback: null,
+
+		/**
+		 * The vehicle position marker
+		 */
+		positionMarker: null,
+		data: null,
+
 		drawVehicleMarker: function (JSONdata) {
 			var markerIcon = L.icon({
 				iconUrl: './img/marker-36.png',
@@ -30,31 +27,37 @@
 				iconSize: [36, 36]
 			});
 
-			var simulationDataGeoJson = this.convertSimulationDataToGeoJson(JSONdata["simulation"]);
+			this.data = this.convertSimulationDataToGeoJson(JSONdata["simulation"]);
 
-			var playback = new L.Playback(map, simulationDataGeoJson, null, {
-				marker: function () {
-					return {
-						icon: markerIcon,
-						iconAngle: 90
+			if (!this.markerDrawn) {
+				this.playback = new L.Playback(map, this.data, null, {
+					marker: function () {
+						return {
+							icon: markerIcon,
+							iconAngle: 90
+						}
 					}
-				}
-			});
-			playback.addCallback(function(timestamp) {
-				if (!simulationDataGeoJson["data"].hasOwnProperty(timestamp.toString())) {
-					console.error("No direction for " + timestamp);
-					return;
-				}
-				var angle = simulationDataGeoJson["data"][timestamp.toString()]["direction"] * 180 / Math.PI;
+				});
+				this.playback.addCallback(function(timestamp) {
+					if (!simulation.data["data"].hasOwnProperty(timestamp.toString())) {
+						console.error("No direction for " + timestamp);
+						return;
+					}
+					var angle = simulation.data["data"][timestamp.toString()]["direction"] * 180 / Math.PI;
 
-				// ATTENTION: This is an undocumented and unsupported hack to get the marker as it is not exposed via
-				// the official API.
-				playback._trackController._tracks[0]._marker.setIconAngle(angle);
-			});
+					// ATTENTION: This is an undocumented and unsupported hack to get the marker as it is not exposed via
+					// the official API.
+					simulation.playback._trackController._tracks[0]._marker.setIconAngle(angle);
+				});
+				this.markerDrawn = true;
 
-			// Initialize custom control
-			var control = new L.Playback.Control(playback);
-			control.addTo(map);
+				// Initialize custom control
+				this.positionMarker = new L.Playback.Control(this.playback);
+				this.positionMarker.addTo(map);
+			} else {
+				this.playback.clearData();
+				this.playback.setData(simulation.data);
+			}
 		},
 
 		convertSimulationDataToGeoJson: function (data) {
