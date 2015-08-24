8628d0eadd210ac4711b6fc78288661dcc469abe
Andreas Wolf <dev@a-w.io>
2015-08-24 16:09:07 +0200

    [BUGFIX] PID controller time constants use seconds instead of ms

--- src/main/scala/info/andreaswolf/roadhopper/simulation/SignalBasedSimulation.scala
+++ src/main/scala/info/andreaswolf/roadhopper/simulation/SignalBasedSimulation.scala
@@ -67,7 +67,7 @@ class SignalBasedSimulation(val route: Route, override val result: SimulationRes
 	val velocityEstimator = actorSystem.actorOf(Props(new TargetVelocityEstimator(signalBus)))
 	val journey = actorSystem.actorOf(Props(new SignalsJourneyActor(timer, signalBus, route)), "journey")
 	val driver = actorSystem.actorOf(Props(new VelocityController(signalBus)))
-	val velocityController = actorSystem.actorOf(Props(new PIDController("v_diff", "alpha_in", -0.0069, -2.59e-6, 5.35e-5, signalBus)))
+	val velocityController = actorSystem.actorOf(Props(new PIDController("v_diff", "alpha_in", -0.0069, -2.59e-3, 5.35e-8, signalBus)))
 	val gasPedal = actorSystem.actorOf(Props(new PT1("alpha_in", "alpha", 10, 500.0, 0.0, signalBus)))
 
 	private val loggedSignals: mutable.Map[Int, SignalState] = new mutable.HashMap[Int, SignalState]()
