cc48d693bb34740a94dc89f1f025e5a1b791d801
Andreas Wolf <dev@a-w.io>
2015-09-04 21:20:46 +0200

    [BUGFIX] Remove duration from Measurement class
    The additional property causes an error when serializing an object for
    the database. The error also occurs when using a def instead of a lazy
    val, so we remove it altogether for now.
    
    Probably an implicit conversion to an object that has the required
    method would work, or adding the method via some Scala magic.

--- src/main/scala/info/andreaswolf/roadhopper/server/MeasurementsServlet.scala
+++ src/main/scala/info/andreaswolf/roadhopper/server/MeasurementsServlet.scala
@@ -74,7 +74,7 @@ class MeasurementsServlet extends BaseServlet {
 
 			json.`object`()
 
-			json.key("duration").value(measurementObject.duration)
+			//json.key("duration").value(measurementObject.duration)
 
 			json.key("measurements").value(convertMeasurementsFile(measurementObject))
 
