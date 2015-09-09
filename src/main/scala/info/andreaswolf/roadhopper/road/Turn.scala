c41c83cc906e5b83c92f2672ebc38d1fbc145a23
Andreas Wolf <dev@a-w.io>
2015-09-09 18:10:27 +0200

    [BUGFIX] Speed limits not correctly transferred to turn segments

--- src/main/scala/info/andreaswolf/roadhopper/road/Turn.scala
+++ src/main/scala/info/andreaswolf/roadhopper/road/Turn.scala
@@ -16,23 +16,25 @@ object Turn {
 
 		// TODO move the speed limit to the velocity estimator, just include a turn-degrees indicator in the turn segment
 		// instead. The estimator should then find an appropriate target velocity based on the driver preferences
+		// The target velocity should also not be fixed, but instead depend on the allowed velocity on the segment before;
+		// otherwise the slowdown might get too harsh e.g. on highway exits in Germany
 		val speedLimit = Math.abs(from.calculateNecessaryTurn(to)).toDegrees match {
-			case x if x > 110 => 7
-			case x if x > 80 => 10
-			case x if x > 45 => 15
-			case x if x > 30 => 25
+			case x if x > 110 => 7 / 3.6
+			case x if x > 80 => 10 / 3.6
+			case x if x > 60 => 15 / 3.6
+			case x if x > 30 => 25 / 3.6
 			case x => from.speedLimit
 		}
 
 		if (from.length - beforeTurnLength < 1.0) {
 			// if we would get a very short segment (< 1m) and the pre-turn segment, we skip the first segment
-			return Seq(new PreTurnSegment(from.start, from.end, speedLimit / 3.6))
+			return Seq(new PreTurnSegment(from.start, from.end, speedLimit))
 		}
 
 		// get the segment until 5 meters before the turn point
 		val beforeTurn = RoadSegment.fromExisting(from, beforeTurnLength)
 		// get the segment right before the turn point
-		val turnBase = new PreTurnSegment(beforeTurn.end, from.end, speedLimit / 3.6)
+		val turnBase = new PreTurnSegment(beforeTurn.end, from.end, speedLimit)
 
 		// filter out a zero-length segment that might occur if the from-segment was shorter than the slowdown distance
 		// of 5 meters
